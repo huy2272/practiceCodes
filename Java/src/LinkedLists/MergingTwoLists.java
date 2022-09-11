83853761b564b86c9c5d6c49b22fd377a518a034
huy227 <huy2272@gmail.com>
2022-09-11 13:07:20 -0400

    Added comments to explain nodes implementation

--- Java/src/LinkedLists/MergingTwoLists.java
+++ Java/src/LinkedLists/MergingTwoLists.java
@@ -21,6 +21,7 @@ public class MergingTwoLists {
 	public ListNode merge(ListNode list1, ListNode list2) {
 		ListNode curr1 = list1;
 		ListNode curr2 = list2;
+		//Instead of a pointer, we are making a dummy Node
 		ListNode head = new ListNode(-1);
 		ListNode temp = head;
 		
@@ -46,11 +47,15 @@ public class MergingTwoLists {
 		}
 
 		while (curr2 != null) {
+			//Make the next pointer points to curr2 Node
 			head.next = curr2;
+			//Move on to the next Node in curr2
 			curr2 = curr2.next;
+			//This moves the pointer to the next new Node
 			head = head.next;
 		}
 
+		//Since we used a dummy Node, the first node contains dummy data, so return temp.next
 		return temp.next;
 	}
 }

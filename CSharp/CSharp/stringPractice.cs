c00a601b851b233c4ff3803e9e539d5ea3ac37e7
huy227 <huy2272@gmail.com>
2022-09-15 19:29:00 -0400

    C# string methods
    Practing string methods in C#

---
+++ CSharp/CSharp/stringPractice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class stringPractice
+{
+    static void Main(string[] args)
+    {
+        var fullname = "Huy Nguyen";
+        Console.WriteLine("Trim: '{0}'", fullname.ToUpper().Trim());
+
+        var index = fullname.IndexOf(' ');
+        var first = fullname.Substring(0, index);
+        var last = fullname.Substring(index + 1);
+
+        Console.WriteLine("firstname: " + first);
+        Console.WriteLine("lastname: " + last);
+
+        var names = fullname.Split(' ');
+        Console.WriteLine("Firstname: " + names[0]);
+        Console.WriteLine("Lastname: " + names[1]);
+
+        var newName = fullname.Replace("Nguyen", "Tang");
+        Console.WriteLine("New name: " + newName);
+
+        if (string.IsNullOrEmpty(" ".Trim()))
+            Console.WriteLine("Invalid");
+        float price = 29.95f;
+        Console.WriteLine(price.ToString("C0"));
+    }
+
+}

055153602c3402316609e27ea9f8f3d50369fe48
huy227 <huy2272@gmail.com>
2022-09-17 21:41:17 -0400

    Finding the length of the last word in a string with white spaces
    Given a string s consisting of words and spaces, return the length of the last word in the string.
    
    A word is a maximal substring consisting of non-space characters only.

---
+++ Java/src/Algorithm/lengthOfLastWord.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+public class lengthOfLastWord {
+
+	public static int length(String s) {
+        boolean flag = false;
+        int counter = 0;
+        for (int i = s.length() - 1; i >= 0; i-- ){
+            if (s.charAt(i) == ' '){
+                //If there is white spaces at the end of the string, it will not return 0
+                if  (flag) {break;}
+            } 
+            else {
+                //If the character at the given index is not empty, start counting
+                //Since we started counting we can set the flag to true
+                //Loop will now terminate once we reach the next empty space
+                counter++;
+                flag = true;
+            }
+        }
+        System.out.println("Length of last word is: " + counter);
+        return counter;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		lengthOfLastWord.length("Hello World");
+	}
+
+}

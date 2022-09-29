0711e396fa3ddcecfbc8beb9e9e6b278ad8a0e10
huy227 <huy2272@gmail.com>
2022-09-28 20:12:57 -0400

    Search in a Binary Search Tree
    You are given the root of a binary search tree (BST) and an integer val.
    
    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.

---
+++ Java/src/Algorithm/SrchBinaryTree.java
@@ -0,0 +1,18 @@
+package Algorithm;
+
+public class SrchBinaryTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {return null;}
+        
+        if  (root.val == val){
+            return root;
+        }
+        
+        TreeNode left = searchBST(root.left, val);
+        if  (left != null){
+            return left;
+        }
+        
+        return searchBST(root.right, val);
+    }
+}

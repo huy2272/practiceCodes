bebb665ebe5535a404f267949bad52140d6a36ad
huy227 <huy2272@gmail.com>
2022-09-28 20:49:38 -0400

    Update SrchBinaryTree.java
    More readable solution for binary tree search.

--- Java/src/Algorithm/SrchBinaryTree.java
+++ Java/src/Algorithm/SrchBinaryTree.java
@@ -1,18 +1,19 @@
 package Algorithm;
 
 public class SrchBinaryTree {
-    public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {return null;}
+	public TreeNode searchBST(TreeNode root, int val) {
+        //If key is found or root is null return the root
+        if (root == null || root.val == val) {return root;}
         
-        if  (root.val == val){
-            return root;
+        //For a BST the right node is always bigger than the root
+        //While the left node is smaller than the root
+        //Therefore if the key if bigger than the root we go right
+        if  (root.val < val){
+            return searchBST(root.right, val);
         }
         
-        TreeNode left = searchBST(root.left, val);
-        if  (left != null){
-            return left;
-        }
-        
-        return searchBST(root.right, val);
+        //If the key is not bigger, then it is smaller
+        //We go search the left hand side
+        return searchBST(root.left, val);
     }
 }

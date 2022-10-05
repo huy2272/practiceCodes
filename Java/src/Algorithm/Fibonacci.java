f9169f3aa991a25da796ecab443c696ebd227f3f
huy227 <huy2272@gmail.com>
2022-10-05 19:54:00 -0400

    Added some comments

--- Java/src/Algorithm/Fibonacci.java
+++ Java/src/Algorithm/Fibonacci.java
@@ -6,21 +6,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Fibonacci {
-	static int fib(int n) {
-		if	(n <= 1) {return n;}
-		
-		int[] arr = new int[n+1];
-		
-		arr[0]=0;
-		arr[1]=1;
-		
-		for	(int i = 2; i <= n; i++) {
-			arr[i] = arr[i-1] + arr[i-2];
-		}
-		
-		return arr[n];
-	}
+//	static int fib(int n) {
+//		if	(n <= 1) {return n;}
+//		
+//		int[] arr = new int[n+1];
+//		
+//		arr[0]=0;
+//		arr[1]=1;
+//		
+//		for	(int i = 2; i <= n; i++) {
+//			arr[i] = arr[i-1] + arr[i-2];
+//		}
+//		
+//		return arr[n];
+//	}
 	
+	//Find the oddonacci sequence iteratively 
 	static int oddonacci(int n) {
 		if	(n <= 2) {return n;}
 		
@@ -30,6 +31,8 @@ public class Fibonacci {
 		arr[1]=1;
 		arr[2]=1;
 		
+		//Since we are iterating to n and we only have one for loop
+		//The time complexity should be O(n)
 		for	(int i = 3; i <= n; i++) {
 			arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
 		}
@@ -37,10 +40,15 @@ public class Fibonacci {
 		return arr[n];
 	}
 	
+	
 	static int oddonacciRecursive(int n) {
+		//We set the first 3 elements
+		//Then by using these base elements we can calculate the rest of the sequence
 		if(n==0) return 1;
         if(n==1) return 1;
         if(n==2) return 1;
+        //Here the time complexity is horrible, it is exponential it should be 3^n (Ternary recursion???) 
+        //To improve we have to use linear recursion
         else 
             return oddonacciRecursive(n-1)+oddonacciRecursive(n-2)+oddonacciRecursive(n-3);
 	}

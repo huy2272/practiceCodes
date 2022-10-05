4d78e953f7b99c539a4a37b07f73bd098d3b8113
huy227 <huy2272@gmail.com>
2022-10-05 19:48:51 -0400

    Calling oddonacci recursively
    The time complexity should be 3^n
    Also we are now writing the program output to a text file called OddoOut.txt

--- Java/src/Algorithm/Fibonacci.java
+++ Java/src/Algorithm/Fibonacci.java
@@ -1,5 +1,10 @@
 package Algorithm;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Fibonacci {
 	static int fib(int n) {
 		if	(n <= 1) {return n;}
@@ -32,11 +37,33 @@ public class Fibonacci {
 		return arr[n];
 	}
 	
+	static int oddonacciRecursive(int n) {
+		if(n==0) return 1;
+        if(n==1) return 1;
+        if(n==2) return 1;
+        else 
+            return oddonacciRecursive(n-1)+oddonacciRecursive(n-2)+oddonacciRecursive(n-3);
+	}
+	
 	public static void main(String[] args) {
 		Fibonacci test = new Fibonacci();
-		//System.out.println(test.fib(3));
-		System.out.println(test.oddonacci(5));
-		System.out.println(test.oddonacci(10));
-		System.out.println(test.oddonacci(15));
+		try {
+			File obj = new File("OddoOut.txt");
+			if (obj.createNewFile()) {System.out.println("File created: " + obj.getName());} 
+			else {System.out.println("File already exists.");}
+			
+			FileOutputStream fout = new FileOutputStream("OddoOut.txt");
+			PrintWriter writer = new PrintWriter(fout);
+			for (int i = 5; i <= 20; i=i+5) {
+				writer.write(String.valueOf(test.oddonacciRecursive(i)));
+				writer.println();
+			}
+			writer.flush();
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println("An error occurred when trying to write to a file");
+			e.printStackTrace();
+		}
 	}
 }

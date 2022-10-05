698a10ecf192fde9919dec3b06ff8e236b1b794d
huy227 <huy2272@gmail.com>
2022-10-04 22:07:31 -0400

    Implement Queue using Stacks
    Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).

---
+++ Java/src/Stacks/QueueUsingStacks.java
@@ -0,0 +1,35 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class QueueUsingStacks {
+	class MyQueue {
+	    Stack<Integer> input;
+	    Stack<Integer> output;
+	    public MyQueue() {
+	        input=new Stack<Integer>();
+	        output=new Stack<Integer>(); 
+	    }
+	    
+	   public void push(int x) {
+	        input.push(x);
+	    }
+
+	   public int pop() {
+	        peek();
+	        return output.pop();
+	    }
+
+	    public int peek() {
+	        if (output.empty())
+	            while (!input.empty())
+	                output.push(input.pop());
+	        return output.peek();
+	    }
+
+	    public boolean empty() {
+	        return input.empty() && output.empty();
+	    }
+
+	}
+}

c385a79ae9437b11d824357efbd5d5538544a495
huy227 <huy2272@gmail.com>
2022-10-04 20:36:01 -0400

    Fixed a bug when code does not work for arr of size 2

--- Java/src/Arrays/ReverseLeftAdditionRight.java
+++ Java/src/Arrays/ReverseLeftAdditionRight.java
@@ -3,45 +3,49 @@ package Arrays;
 import java.util.Arrays;
 
 public class ReverseLeftAdditionRight {
-	
+
 	static int[] reverseAndAdd(int[] arr) {
-		int mid = arr.length/2;
-		if(arr.length%2==0) {
-			for	(int i = 0; i < mid ; i++) {
-				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
-				i++;
-			}
-			
-			for	(int i = mid; i < arr.length; i++ ) {
-				int temp = arr[i];
-				arr[i+1] += temp;
-				i++;
-			}
+		int mid = arr.length / 2;
+		if (arr.length <= 2 && arr.length >= 1) {
+			System.out.println(Arrays.toString(arr));
+			return arr;
 		}
-		else {
-			int oddMid = mid-1;
-			for	(int i = 0; i < oddMid ; i++) {
+		if (arr.length % 2 == 0) {
+			for (int i = 0; i < mid; i++) {
 				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
 				i++;
 			}
-			
-			for	(int i = mid+1; i < arr.length-1; i++ ) {
+
+			for (int i = mid; i < arr.length; i++) {
 				int temp = arr[i];
-				arr[i+1] += temp;
+				arr[i + 1] += temp;
+				i++;
+			}
+		} else {
+			int oddMid = mid - 1;
+			for (int i = 0; i < oddMid; i++) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				i++;
+			}
+
+			for (int i = mid + 1; i < arr.length - 1; i++) {
+				int temp = arr[i];
+				arr[i + 1] += temp;
 				i++;
 			}
 		}
 		System.out.println(Arrays.toString(arr));
 		return arr;
 	}
-	
+
 	public static void main(String[] args) {
-		int[] test1 = {1,2,3,4,5,1,2,3,4};
+		int[] test1 = { 0, 1 };
 		ReverseLeftAdditionRight test = new ReverseLeftAdditionRight();
 		test.reverseAndAdd(test1);
+
 	}
 }

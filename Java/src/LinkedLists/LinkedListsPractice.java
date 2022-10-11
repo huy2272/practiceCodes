b3ba294d3841b16f2ed60df171573b93f9f7d3ba
huy227 <huy2272@gmail.com>
2022-10-11 15:03:22 -0400

    Changed some variable names to make code more readable

--- Java/src/LinkedLists/LinkedListsPractice.java
+++ Java/src/LinkedLists/LinkedListsPractice.java
@@ -10,31 +10,33 @@ public class LinkedListsPractice {
 	
 	
 	public static void main(String[] args) {
-//		LinkedList<Integer> list = new LinkedList<>();
-//		
-//		list.add(7);
-//		list.add(8);
-//		list.add(9);
-//		list.remove();
-//		System.out.println(list.get(1));
-//		
-//		//Iterate through the list
-//		Iterator<Integer> iterate = list.iterator();
-//		while(iterate.hasNext()) {
-//			if	((int) iterate.next() == 8) {
-//				System.out.println("We found 8");
-//			}
-//		}
+		LinkedList<Integer> list = new LinkedList<>();
 		
-		ListNode a = new ListNode(1);
-		ListNode b = new ListNode(0);
-		ListNode c = new ListNode(0);
-		ListNode d = new ListNode(1);
-		a.next = b;
-		b.next = c;
-		c.next = d;
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		//Remove the head node of the linked list
+		list.remove();
+		System.out.println(list);
+		System.out.println(list.get(0));
 		
-		Palindrome p = new Palindrome();
-		p.isPalindrome(a);
+		//Iterate through the list
+		Iterator<Integer> iterate = list.iterator();
+		while(iterate.hasNext()) {
+			if	((int) iterate.next() == 8) {
+				System.out.println("We found 8");
+			}
+		}
+		
+//		ListNode a = new ListNode(1);
+//		ListNode b = new ListNode(0);
+//		ListNode c = new ListNode(0);
+//		ListNode d = new ListNode(1);
+//		a.next = b;
+//		b.next = c;
+//		c.next = d;
+//		
+//		Palindrome p = new Palindrome();
+//		p.isPalindrome(a);
 	}
 }
--- Java/src/LinkedLists/MergingTwoLists.java
+++ Java/src/LinkedLists/MergingTwoLists.java
@@ -6,40 +6,40 @@ public class MergingTwoLists {
 		ListNode curr1 = list1;
 		ListNode curr2 = list2;
 		//Instead of a pointer, we are making a dummy Node
-		ListNode head = new ListNode(-1);
-		ListNode temp = head;
+		ListNode temp = new ListNode(-1);
+		ListNode head = temp;
 		
 		while (curr1 != null && curr2 != null) {
 			if (curr1.val < curr2.val) {
 				// If value in list 1 is smaller than list 2, have the dummy point at curr1
 				// Have the curr1 listNode next pointer point at the next listNode
-				head.next = curr1;
+				temp.next = curr1;
 				curr1 = curr1.next;
 			} else {
 				// Have the dummy point at curr2 if curr2 listNode value is smaller
-				head.next = curr2;
+				temp.next = curr2;
 				curr2 = curr2.next;
 			}
 			
-			head = head.next;
+			temp = temp.next;
 		}
 
 		while (curr1 != null) {
-			head.next = curr1;
+			temp.next = curr1;
 			curr1 = curr1.next;
-			head = head.next;
+			temp = temp.next;
 		}
 
 		while (curr2 != null) {
 			//Make the next pointer points to curr2 Node
-			head.next = curr2;
+			temp.next = curr2;
 			//Move on to the next Node in curr2
 			curr2 = curr2.next;
 			//This moves the pointer to the next new Node
-			head = head.next;
+			temp = temp.next;
 		}
 
 		//Since we used a dummy Node, the first node contains dummy data, so return temp.next
-		return temp.next;
+		return head.next;
 	}
 }

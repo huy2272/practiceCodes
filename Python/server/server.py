efbaad4bfbd87ac564b6a91aab3c44f8b49106cd
huy227 <huy2272@gmail.com>
2022-11-16 18:14:50 -0500

    Set up server and client using SOCK_STREAM AND AF_INET

--- Python/server/server.py
+++ Python/server/server.py
@@ -1,4 +1,18 @@
 import socket
 
 #If local host doesn't work use 127.0.0.1
-HOST, PORT = "localhost", 9999
+HOST, PORT = "127.0.0.1", 65432
+
+#AF_INET is the Internet family for IPv4
+#SOCK_STREAM is the sokcet type for TCP, protocol that is used to transport messages
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)

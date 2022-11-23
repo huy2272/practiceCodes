2e5f6a1c4b3af66cfd89f9a3c8fd4a583e0479df
huy227 <huy2272@gmail.com>
2022-11-22 22:46:35 -0500

    Created the starting point on the client side

--- Python/client/client.py
+++ Python/client/client.py
@@ -1,6 +1,7 @@
 import socket
 
-HOST, PORT = "127.0.0.1", 65432
+HOST = "127.0.0.1" 
+PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -8,3 +9,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
 
 print(f"Received{data!r}")
+
+'''
+Entry point
+'''
+def main():
+    print("Assignment 2")
+
+if __name__ == "__main__":
+    main()
